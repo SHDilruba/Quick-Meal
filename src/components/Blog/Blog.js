6e22b32cbd8e5326fac01703e709b6efd2437897
unknown <hasinadilruba2021@gmail.com>
2022-11-20 23:47:02 +0100

    blog page done

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -3,26 +3,25 @@ import Post from '../Post/Post';
 import './Blog.css';
 
 const Blog = () => {
-  // const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
 
-  // useEffect(() =>{
-  //      fetch('')
-  //      .then(res => res.json())
-  //      .then(data => setPosts(data))
-  // },[])
+  useEffect(() =>{
+       fetch('http://localhost:5000/blog')
+       .then(res => res.json())
+       .then(data => setPosts(data))
+  },[])
 
   return (
     <div className="container blog-container">
-        <h2 className='blog text-center mt-5 pt-5 ps-3'>Blog about Technology</h2>
+        <h2 className='blog text-center'>Blog about Technology</h2>
          <div className="post-container">
-          <Post></Post>
-            {/* {
+            {
                posts.map(post => <Post 
                 post={post} 
-                key={post.id}
+                key={post._id}
                 ></Post> 
                 ) 
-            } */}
+            }
          </div>
     </div>
   );
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import './Post.css'
+import { Card } from 'react-bootstrap';
 
-const Post = () => {
-  // const { name, description} = props.post;
+const Post = ({post}) => {
+  console.log(post)
+  const {question, answer} = post;
 
   return (
-    <div>
-       <div className="card-box container">
-  <div className="box bg-light">
-    <div className="body">
-      <h4 className="card-title mb-2">name</h4>
-      <p className="card-text">description</p>
-    </div>
-   </div>
- </div>
-</div>
-  );
-};
+       <div className=" container">
+            <Card id='blog-card' border="dark">
+               <Card.Header className='card-header py-5 bg-dark text-warning py-4'>{question}</Card.Header>
+                  <Card.Body className='card-text'>
+                      <Card.Text>
+                        {answer}
+                      </Card.Text>
+                  </Card.Body>
+           </Card>
+       </div>
+     );
+  };
 
 export default Post;
